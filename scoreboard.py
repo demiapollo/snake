700df0b7b2eeae1a7aa78fa8578637c8d2934866
demiapollo <apollodemirel67@gmail.com>
2023-11-10 19:02:19 -0800

    added a text file to store high score that persists after closing and reopening the game.

--- scoreboard.py
+++ scoreboard.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
@@ -10,7 +11,8 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.update_scoreboard()
 
@@ -21,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
     
